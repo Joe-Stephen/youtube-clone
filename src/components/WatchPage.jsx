621b863a143f65a05cfc1ac32739d07d2bc48a70
Joe Stephen <joestephenk10@gmail.com>
2024-08-13 15:29:21 +0530

    Autoplaying video in the view page.

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { closeMenu, toggleMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import {
+  YOUTUBE_WATCH_URL_POSTFIX,
+  YOUTUBE_WATCH_URL_PREFIX,
+  YOUTUBE_WATCH_VIDEO_URL,
+} from "../utils/constants";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -15,7 +20,7 @@ const WatchPage = () => {
       <iframe
         width="640"
         height="360"
-        src={`https://www.youtube.com/embed/${videoId}?si=gtB-hpGB8LbuOrKU`}
+        src={YOUTUBE_WATCH_URL_PREFIX + videoId + YOUTUBE_WATCH_URL_POSTFIX}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
